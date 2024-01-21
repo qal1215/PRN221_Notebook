cbdaed9313de3ee0fb7ba0f2b4f4a10f9db1fd25
qal <lequyetanh@outlook.com>
2024-01-22 06:26:17 +0700

    005663

--- ManageSchoolScore/Repository/Repository.cs
+++ ManageSchoolScore/Repository/Repository.cs
@@ -171,7 +171,7 @@ namespace ManageSchoolScore.Repository
 
         private static async Task BulkInsertStudentNSubject(IEnumerable<Student> studentList, List<Score> scoreList)
         {
-            var CommitBatchSize = 150000;
+            var CommitBatchSize = 170000;
             using (var context = new DBContextMSS())
             {
                 await context.BulkInsertAsync(studentList);
